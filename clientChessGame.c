2a66fe982a1ac290ef42ce3331b92205e0134eb6
me <me@newtinydebian.lan>
2014-04-16 10:13:27 +0200

    chanigne names

---
+++ clientChessGame.c
@@ -0,0 +1,320 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+#include <netdb.h>
+#include <netinet/in.h>
+
+//#define SOCK_PATH "/tmp/labnet_socket"
+//#define MAX_BYTES 4096
+//******** net additions
+
+#define PORT 3490 // the port client will be connecting to
+#define MAXDATASIZE 100 // max number of bytes we can get at once
+
+#define WHITE        1
+#define BLACK        2
+
+#define wPAWN        10
+#define wROOK        11
+#define wKNIGHT      12
+#define wBISHOP      13
+#define wQUEEN       14
+#define wKING        15
+
+#define bPAWN        20
+#define bROOK        21
+#define bKNIGHT      22
+#define bBISHOP      23
+#define bQUEEN       24
+#define bKING        25
+
+int movedPiece=1;
+int sendTable=0;
+int winner=0;
+int connectSocket;
+int colour;
+
+int main(int argc, char *argv[])
+{
+    int x,y,nx,ny,*pawn,*rook,*bishop,*knight,*queen,*king, *currentPlayer=20, *playerVar1, *playerVar2, *playerVar3;
+    int chessBoardArray[8][8];
+    int errorNum=0;
+    char playerName[100];
+
+        //TCP VARS
+    char buf[MAXDATASIZE];
+    struct hostent *he;
+    struct sockaddr_in their_addr; // connector's address information
+    struct sockaddr_un remote;
+
+
+
+    if (argc != 2) // if more than two commands
+    {
+        fprintf(stderr,"usage: client hostname\n");
+        exit(1);
+    }
+    if((connectSocket= socket(AF_INET, SOCK_STREAM, 0))==-1)
+    {
+        perror("socket");
+        exit(1);
+    }
+
+    if ((he=gethostbyname(argv[1])) == NULL)    // get the host info
+    {
+        perror("gethostbyname");
+        exit(1);
+    }
+    //strcpy((*he), "130.237.84.198");
+
+// START main Code
+// TCP connect code:
+    printf("Trying to connect ...\n");
+
+    their_addr.sin_family = AF_INET;    // host byte order
+    their_addr.sin_port = htons(PORT);  // short, network byte order
+    their_addr.sin_addr = *((struct in_addr *)he->h_addr);
+    memset(their_addr.sin_zero, '\0', sizeof their_addr.sin_zero);
+
+    if (connect(connectSocket,(struct sockaddr*)&their_addr,sizeof their_addr)==-1)
+    {
+        perror("connect");
+        exit(1);
+    }
+
+    printf("\n");
+    printf("Connected.\n");
+    printf("\n");
+    // Get name and send to server
+    printf("Type your username:");
+    scanf("%s", &playerName);
+
+     printf("\n PlayerName: ->%s<- \n", playerName);
+    if (send(connectSocket, playerName, sizeof(playerName), 0) == -1) {
+        perror("send");
+        exit(1);
+    }
+    recv(connectSocket, &colour, sizeof(colour),0);
+    printf("colour: %d\n", colour);
+
+    printf("sleeping...\n");
+    printf("Waiting for opponent\n");
+    recv(connectSocket, chessBoardArray, sizeof(chessBoardArray), 0);
+
+    for(;;){
+        if(sendTable==1){
+            send(connectSocket, &chessBoardArray, sizeof(chessBoardArray), 0);
+            send(connectSocket, &winner, sizeof(winner), 0);
+            printBoard(chessBoardArray);
+            printf("sleeping...\n");
+            printf("Waiting for opponent\n");
+            recv(connectSocket, chessBoardArray, sizeof(chessBoardArray), 0);
+            sendTable=0;
+        }
+        checkColour(&currentPlayer, &playerVar1, &playerVar2, &playerVar3, &pawn, &rook, &knight, &bishop, &queen, &king);
+        printBoard(chessBoardArray);
+        printf("Spelare %d: Vilken pjas vill du flytta? (y x)", currentPlayer);
+        scanf("%d %d", &y, &x); /* data för vart pjäsen man vill flytta står */
+        if(chessBoardArray[y][x]!=0){
+
+
+            if(chessBoardArray[y][x]==pawn){tvaBonde(chessBoardArray, x, y, currentPlayer, playerVar1, playerVar2, playerVar3);}
+            else if(chessBoardArray[y][x]==rook){tvaTorn(chessBoardArray, x, y, currentPlayer, playerVar1, playerVar2, playerVar3);}
+            /*else if(chessBoardArray[y][x]==knight){tvaHast(chessBoardArray, x, y);}
+            else if(chessBoardArray[y][x]==bishop){tvaLopare(chessBoardArray, x, y);}
+            else if(chessBoardArray[y][x]==queen){tvaDrottning(chessBoardArray, x, y);}
+            else if(chessBoardArray[y][x]==king){tvaKung(chessBoardArray, x, y);}      */
+            else{
+                printf("\nEj rorlig pjas\n");
+                sendTable=0;
+            }
+        }
+        else{
+            printf("\nTom ruta\n");
+            sendTable=0;
+        }
+    }
+    return 0;
+}
+
+void tvaBonde(int chessBoardArray[8][8],int x,int y, int currentPlayer, int playerVar1, int playerVar2, int playerVar3, int nx, int ny, int pawnDouble){
+        for(;;){
+            pawnDouble=playerVar3*2;
+            printf("Bonde: Vart vill du flytta pjasen?\n");
+            scanf("%d %d", &ny, &nx); /* data för vart pjäses ska flyttas */
+            if(ny==y+playerVar3 && nx==x && chessBoardArray[ny][nx]==0){ /* Kollar om rutan framför bonden är tom */
+                validMove(chessBoardArray,x,y,nx,ny);
+                return;
+            }
+            else if(ny==y+pawnDouble && chessBoardArray[ny][nx]==0 && (y==6 || y==1)){
+                validMove(chessBoardArray,x,y,nx,ny);
+                return;
+            }
+            else if(ny==y+playerVar3 && (nx==x+1 || nx == x-1) && chessBoardArray[ny][nx]>playerVar1 && chessBoardArray[ny][nx]<playerVar2){ /* Kollar ifall användaren kan ta ut en av motståndarens pjäser */
+                validMove(chessBoardArray,x,y,nx,ny);
+                return;
+            }
+            else{
+                printf("\nInvalid move\n");
+            }
+        }
+}
+
+void tvaTorn(int chessBoardArray[8][8],int x,int y, int currentPlayer, int playerVar1, int playerVar2, int playerVar3, int nx, int ny, int i, int j){
+        for(;;){
+            printf("Torn:\n");
+            printf("Vart vill du flytta pjasen?");
+            scanf("%d %d", &ny, &nx);
+            if(ny==y || nx==x){
+                if(ny==y){ /* Kollar om tornet rörde sig vågrät */
+                    if((x-nx)<0){ /* Kollar om den rörde sig åt höger eller vänster (höger i detta fall) */
+                        j=1;
+                        for(i=nx-x; i>0; i--){ /* kontrollerar i for-loopen om alla rutor i dess väg är tomma */
+                            if(chessBoardArray[y][x+j]==0){
+                                if(nx-x==1){ /* Om tornet bara tar 1 steg behöver den inte ktrollera om alla rutor är tomma */
+                                    validMove(chessBoardArray,x,y,nx,ny);
+                                    return;
+                                }
+                                j++;
+                                if(j==nx-x && (chessBoardArray[ny][nx]== 0 ||(chessBoardArray[ny][nx]>playerVar1 && chessBoardArray[ny][nx]<playerVar2))){ /* Om alla rutor den passerar är tomma och destinationen är tom eller har en fiende */
+                                    validMove(chessBoardArray,x,y,nx,ny);
+                                    return;
+                                }
+                            }
+                            else if(nx-x==1 && (chessBoardArray[ny][nx]== 0 ||(chessBoardArray[ny][nx]>playerVar1 && chessBoardArray[ny][nx]<playerVar2))){ /* Om tornet bara tar 1 steg kollar den om de finns en fiende framför */
+                                    validMove(chessBoardArray,x,y,nx,ny);
+                                    return;
+                            }
+                            else{
+                                printf("\nInvalid move\n");
+                            }
+                        }
+                    }
+                    else if((x-nx)>0){ /* Ifall tornet rör sig åt vänster */
+                        j=1;
+                        for(i=x-nx; i>0; i--){
+                            if(chessBoardArray[y][x-j]==0){
+                                if(x-nx==1){
+                                    validMove(chessBoardArray,x,y,nx,ny);
+                                    return;
+                                }
+                                j++;
+                                if(j==x-nx && (chessBoardArray[ny][nx]== 0 ||(chessBoardArray[ny][nx]>playerVar1 && chessBoardArray[ny][nx]<playerVar2))){
+                                    validMove(chessBoardArray,x,y,nx,ny);
+                                    return;
+                                }
+                            }
+                            else if(x-nx==1 && (chessBoardArray[ny][nx]== 0 ||(chessBoardArray[ny][nx]>playerVar1 && chessBoardArray[ny][nx]<playerVar2))){
+                                    validMove(chessBoardArray,x,y,nx,ny);
+                                    return;
+                            }
+                            else{
+                                printf("\nInvalid move\n");
+                            }
+                        }
+                    }
+                }
+                else if(nx==x){ /* Kollar ifall tornet rör sig lodrät */
+                    if((y-ny)<0){ /* Ifall tornet rör sig nedåt */
+                        j=1;
+                        for(i=ny-y; i>0; i--){
+                            if(chessBoardArray[y+j][x]==0){
+                                if(ny-y==1){
+                                    validMove(chessBoardArray,x,y,nx,ny);
+                                    return;
+                                }
+                                j++;
+                                if(j==ny-y && (chessBoardArray[ny][nx]== 0 ||(chessBoardArray[ny][nx]>playerVar1 && chessBoardArray[ny][nx]<playerVar2))){
+                                    validMove(chessBoardArray,x,y,nx,ny);
+                                    return;
+                                }
+                            }
+                            else if(ny-y==1 && (chessBoardArray[ny][nx]== 0 ||(chessBoardArray[ny][nx]>playerVar1 && chessBoardArray[ny][nx]<playerVar2))){
+                                    validMove(chessBoardArray,x,y,nx,ny);
+                                    return;
+                            }
+                            else{
+                                printf("\nInvalid move\n");
+                            }
+                        }
+                    }
+                    else if((y-ny)>0){ /* Ifall tornet rör sig uppåt */
+                        j=1;
+                        for(i=y-ny; i>0; i--){
+                            if(chessBoardArray[y-j][x]==0){
+                                if(y-ny==1){
+                                    validMove(chessBoardArray,x,y,nx,ny);
+                                    return;
+                                }
+                                j++;
+                                if(j==y-ny && (chessBoardArray[ny][nx]== 0 ||(chessBoardArray[ny][nx]>playerVar1 && chessBoardArray[ny][nx]<playerVar2))){
+                                    validMove(chessBoardArray,x,y,nx,ny);
+                                    return;
+                                }
+                            }
+                            else if(y-ny==1 && (chessBoardArray[ny][nx]== 0 ||(chessBoardArray[ny][nx]>playerVar1 && chessBoardArray[ny][nx]<playerVar2))){
+                                    validMove(chessBoardArray,x,y,nx,ny);
+                                    return;
+                            }
+                            else{
+                                printf("\nInvalid move\n");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+}
+
+void printBoard(int chessBoardArray[8][8],int j,int i){
+    for(i=0; i<8; i++){
+            for(j=0; j<8; j++)
+            {
+                printf("\t %d",chessBoardArray[i][j]);
+            }
+            printf("\n\n");
+    }
+    return;
+}
+
+void checkColour(int *currentPlayer, int *playerVar1, int *playerVar2, int *playerVar3, int *pawn, int *rook, int *knight, int *bishop, int *queen, int *king){
+    if(colour==WHITE){
+        *currentPlayer=10;
+        *playerVar1=19;
+        *playerVar2=26;
+        *playerVar3=1;
+        *pawn=10;
+        *rook=11;
+        *knight=12;
+        *bishop=13;
+        *queen=14;
+        *king=15;
+    }
+    else if(colour==BLACK){
+        *currentPlayer=20;
+        *playerVar1=9;
+        *playerVar2=16;
+        *playerVar3=-1;
+        *pawn=20;
+        *rook=21;
+        *knight=22;
+        *bishop=23;
+        *queen=24;
+        *king=25;
+    }
+    movedPiece=0;
+}
+
+void validMove(int chessBoardArray[8][8],int x,int y, int nx, int ny){
+    chessBoardArray[ny][nx]=chessBoardArray[y][x];
+    chessBoardArray[y][x]=0;
+    sendTable=1;
+    movedPiece=1;
+    return;
+}
---
+++ serverChessGame.c
@@ -0,0 +1,205 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <signal.h>
+
+
+#define SOCK_PATH "/tmp/labnet_socket"
+#define FILE_PATH "/tmp/lab_serverpid"
+#define MAXCLIENTS 10
+#define NAMELENGTH 100
+
+#define wPAWN        10
+#define wROOK        11
+#define wKNIGHT      12
+#define wBISHOP      13
+#define wQUEEN       14
+#define wKING        15
+
+#define bPAWN        20
+#define bROOK        21
+#define bKNIGHT      22
+#define bBISHOP      23
+#define bQUEEN       24
+#define bKING        25
+
+//#define MAIN LOG_FILE "/tmp/mainserver.log" // USED FOR LOGGING
+#define PATH "/tmp/"
+
+
+// TCP Defines
+#define MYPORT 3490 // the port users will be connecting to
+#define BACKLOG 10 // how many pending connections queue will hold
+
+/*struct
+{
+
+
+};*/
+
+
+int main(int argc, char *argv[])
+{
+    int connectSock, clientSock, clientSock1, clientSock2; //len, acceptAdressLength
+    int pid;
+    int date; //To be con....
+    char playerOne[100], playerTwo[100];
+    struct sockaddr_in gameServerAddress; //my address info
+    struct sockaddr_in gameClientAddress;
+    socklen_t sin_size;
+    int winner=0;
+    int sizeofClientAddress;
+    struct sockaddr_un local, remote;   //For connection
+    int yes = 1; // TCP connection variable
+    int WHITE=1, BLACK=2;
+
+    //FILE *mainlog = fopen("/tmp/serverlog","w");
+    //Write header for file.
+    //fprintf("date: %d, Player 1: %s, Player 2: %s", date, playerOne, playerTwo);
+
+int chessBoardArray[8][8] = {
+        {wROOK,wKNIGHT,wBISHOP,wQUEEN,wKING,wBISHOP,wKNIGHT,wROOK}, /* spelare 1: torn=11 häst=12 löpare=13 drottning=14 kung=15 */
+        {wPAWN,wPAWN,wPAWN,wPAWN,wPAWN,wPAWN,wPAWN,wPAWN}, /* bonde=10 */
+
+        {0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0},
+
+        {bPAWN,bPAWN,bPAWN,bPAWN,bPAWN,bPAWN,bPAWN,bPAWN}, /* bonde=20 */
+        {bROOK,bKNIGHT,bBISHOP,bQUEEN,bKING,bBISHOP,bKNIGHT,bROOK}  /* spelare 2: torn=21 häst=22 löpare=23 drottning=24 kung=25 */
+};
+    //Start connection
+    if((connectSock = socket(AF_INET, SOCK_STREAM,0)) == -1)
+    {
+        perror("Socket: ");
+        exit(1);
+    }
+    //Connection done
+
+    if(setsockopt(connectSock, SOL_SOCKET, SO_REUSEADDR, &yes,sizeof(int)) == -1)
+    {
+        perror("setsockopt");
+        exit(1);
+    }
+
+    gameServerAddress.sin_family = AF_INET;   //Host byte order.
+    gameServerAddress.sin_port = htons(MYPORT); //Short, network byte order
+    gameServerAddress.sin_addr.s_addr = INADDR_ANY;   //Automatically filled with my IP
+    memset(gameServerAddress.sin_zero, '\0', sizeof gameServerAddress.sin_zero);
+
+    if(bind(connectSock,(struct sockaddr *)&gameServerAddress, sizeof gameServerAddress) == -1)
+    {
+        perror("bind");
+        printf("Bind successful. local-sun_path: %s Exiting\n",local.sun_path);
+        exit(1);
+
+    }
+    if(listen(connectSock, BACKLOG) == -1)
+    {
+        perror("listen");
+        exit(1);
+    }
+
+    //Main-loop
+    for(;;) //Etarnal loop
+    {
+        printf("Server<%d> Waiting for connection...\n",getpid());
+        sizeof gameClientAddress;   //Why here'
+        sin_size = sizeof gameClientAddress;
+
+        if((clientSock1 = accept(connectSock, (struct sockaddr *)&gameClientAddress, &sin_size)) == -1)
+        {
+            perror("accept");
+            continue;
+        }
+        //Receives name from player 1
+        recv(clientSock1, playerOne, sizeof(playerOne),0);
+        send(clientSock1, &WHITE, sizeof(WHITE), 0);
+        printf("Player 1:  >%s<  \n", playerOne);
+        printf("Colour: %d\n", WHITE);
+
+        if((clientSock2 = accept(connectSock, (struct sockaddr *)&gameClientAddress, &sin_size)) == -1)
+        {
+            perror("accept");
+            continue;
+        }
+        recv(clientSock2, playerTwo, sizeof(playerTwo),0);
+        send(clientSock2, &BLACK, sizeof(BLACK), 0);
+        printf("Player 2: >%s< \n",playerTwo);
+        printf("Colour: %d\n", BLACK);
+
+        //FILE *mainlog = fopen("/tmp/","w");
+        //fprintf("date: %d, Player 1: %s, Player 2: %s", date, playerOne, playerTwo);
+        printf("%d server: got connection from\n", getpid());
+
+
+        pid = fork(); // F O R K    H E R E  ! ! ! ! ! ! !
+        if(pid!=0)
+        {
+            printf("DEBUG: Server<%d>  closing Sockets\n", getpid());
+//            close(clientSock1);
+//            close(clientSock2);
+            //fclose(mainlog);
+
+        }
+        if(pid==0) // C H I L D     P R O C C E S S   O N L Y
+        {
+            close(connectSock);
+            printf("c: %d Server says: Connected.\n", getpid());
+            //Main-loop
+            for(;;)
+            {
+
+                printf("DEBUG: Server<%d>  player %s turn\n", getpid(), playerOne);
+
+                printf("DEBUG: Server<%d>  player  %s  send board\n", getpid(), playerOne);
+                send(clientSock1, &chessBoardArray, sizeof(chessBoardArray), 0);
+
+                printf("DEBUG: Server<%d>  player  %s  rec board\n", getpid(), playerOne);
+                recv(clientSock1, &chessBoardArray, sizeof(chessBoardArray), 0);
+
+                printf("DEBUG: Server<%d>  player  %s  rec winner\n", getpid(), playerOne);
+                recv(clientSock1, &winner, sizeof(winner), 0);
+
+                if(winner == 1)
+                {
+                    printf("%s won!\n",playerOne);
+                    //fprintf(mainlog, "%s");
+                    //fclose(mainlog);
+                }
+
+
+                printf("DEBUG: Server<%d>  player %s turn\n", getpid(), playerTwo);
+                send(clientSock2, &chessBoardArray, sizeof(chessBoardArray),0);
+
+                printf("DEBUG: Server<%d>  player  %s  rec board\n", getpid(), playerTwo);
+                recv(clientSock2, &chessBoardArray, sizeof(chessBoardArray),0);
+
+                printf("DEBUG: Server<%d>  player %s rec winner\n", getpid(), playerTwo);
+                recv(clientSock2, &winner, sizeof(winner),0);
+                if(winner == 1)
+                {
+                    printf("%s won!",playerTwo);
+                    //fprintf(mainlog, "%s");
+                    //fclose(mainlog);
+                }
+            }
+
+        }
+
+    }
+    return 0;
+}
